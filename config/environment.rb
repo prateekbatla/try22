3013cedccb1ce0c78d1af38087431b3ffcd66800
Prateek Batla <batla@adobe.com>
2013-10-17 08:36:17 +0530

    initialize repo

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Tryy2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tryy2::Application.config.secret_token = 'b8f03d4f0ac54af5e48e052e00084a83543f596896522df0ae399db3cc70f848d77feb6326a958f008da0fa711ae1762b0454c2e2631beb70293e3ccecaff455'
